477504d5594c0d810ca3a20985a551de6c527238
reesmcivor <reesmcivor@hotmail.com>
2023-04-29 22:28:15 +0100

    Auto

--- database/migrations/tenant/2023_04_29_000001_create_user_roles_table.php
+++ database/migrations/tenant/2023_04_29_000001_create_user_roles_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use ReesMcIvor\Auth\Models\Role;
 use ReesMcIvor\Forms\Models\Form;
 use ReesMcIvor\Forms\Models\FormEntry;
 use ReesMcIvor\Forms\Models\Question;
@@ -14,7 +15,7 @@ return new class extends Migration
         Schema::create('user_roles', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(\App\Models\User::class);
-            $table->foreignIdFor(\App\Models\Role::class);
+            $table->foreignIdFor(Role::class);
             $table->timestamps();
         });
     }
