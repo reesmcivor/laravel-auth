b3b8515e69365a823beaade278dfd7ae395a583b
reesmcivor <reesmcivor@hotmail.com>
2023-04-30 10:38:25 +0100

    Auto

--- src/routes/tenant.php
+++ src/routes/tenant.php
@@ -8,17 +8,13 @@ use ReesMcIvor\Auth\Http\Controllers\Api\RegisterController;
 use ReesMcIvor\Auth\Http\Controllers\Api\ResetPasswordController;
 use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 
-Route::group([
-    'middleware' => [
-        'tenant',
-        'api',
-        PreventAccessFromCentralDomains::class
-    ], // See the middleware group in Http Kernel
-], function () {
-
+Route::middleware([
+    'api',
+    \Stancl\Tenancy\Middleware\InitializeTenancyByDomainOrSubdomain::class,
+    PreventAccessFromCentralDomains::class,
+])->group(function () {
     Route::post('user/forgot_password', ForgotPasswordController::class);
     Route::post('user/set_password', ResetPasswordController::class);
     Route::post('user/login', LoginController::class);
-    Route::post('user/register', RegisterController::class);
-
+    Route::post('api/user/register', RegisterController::class);
 });
